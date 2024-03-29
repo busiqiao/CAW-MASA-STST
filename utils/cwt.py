41827a0b1a3f1fc85724c97a82f38e14185db5a3
busiqiao <busiqiao666@gmail.com>
2024-03-29 09:25:24 +0800

    添加CWT变换函数

--- utils/cwt.py
+++ utils/cwt.py
@@ -1,47 +1,62 @@
 import numpy as np
-import scipy.signal
+from scipy.signal import cwt, morlet
 import scipy.io
+import matplotlib.pyplot as plt
 
-def morse_wavelet(M, beta, gamma):
-    # 定义Morse wavelet
-    t = np.linspace(-1, 1, M)
-    wavelet = (beta ** 0.5) * np.exp(-beta * (t ** 2)) * np.exp(1j * 2 * np.pi * gamma * t)
-    return wavelet
-
-def wavelet_func(t, width):
-    return morse_wavelet(20, 3.5, 3.5)  # 使用自定义的Morse wavelet
 
 def transform():
-    for f in range(1, 2):
+    savePath = 'H:\\EEG\\EEGDATA\\EEG72-CWT\\'
+
+    data_shape = (5188, 124, 32)
+
+    # 设置连续小波变换的参数
+    widths = np.arange(1, 51)  # 尺度参数范围
+
+    # 创建空数组来存储结果
+    transformed_data = np.zeros((data_shape[0], data_shape[1], len(widths), data_shape[2]), dtype=np.complex64)
+
+    for f in range(1, 11):
         # 读取数据
-        file_path = f'H:\\EEG\\EEGDATA\\S{f}.mat'
+        file_path = f'H:\\EEG\\EEGDATA\\EEG72\\S{f}.mat'
         mat = scipy.io.loadmat(file_path)
         raw_data = np.asarray(mat['X_3D'])
         raw_data = np.transpose(raw_data, (2, 0, 1))
 
-        # 定义频率范围
-        scales = np.arange(1, 21)
-
-        # 初始化一个用于存储结果的数组
-        cwt_data = np.empty((5188, 124, 20, 32))
-
-        # 对每次测试的数据进行小波变换
-        for i in range(5188):
-            for j in range(124):
-                cwt_result = scipy.signal.cwt(raw_data[i, j, :], wavelet_func, scales)
-                cwt_data[i, j, :, :] = cwt_result
-                # print(f'第{i}个测试的第{j}个通道的小波变换完成')
+        # 对每个通道进行连续小波变换
+        for i in range(data_shape[0]):
+            for j in range(data_shape[1]):
+                signal = raw_data[i, j, :]
+                # 进行连续小波变换
+                coefficients = cwt(signal, morlet, widths)  # 使用Morlet小波作为基函数
+                transformed_data[i, j, :, :] = coefficients
 
         # 保存结果
-        np.save(f'H:\\EEG\\EEGDATA\\cwt\\S{f}_cwt_data.npy', cwt_data)
-        print(f'S{f} done.')
+        np.save(savePath + f'S{f}_CWT.npy', transformed_data)
+        print(f'S{f} done')
+
+
 def test():
+    data_shape = (5188, 124, 32)
+
     # 读取数据
-    file_path = 'H:\\EEG\\EEGDATA\\cwt\\S1_cwt_data.npy'
-    cwt_data = np.load(file_path)
-    print(cwt_data.shape)
+    path = 'continuous_wavelet.npy'
+    transformed_data = np.load(path)
+
+    # 可视化结果（这部分可选）
+    # 选择一个通道的连续小波变换结果进行可视化
+    channel_index = 0  # 选择第一个通道进行可视化
+    expt_index = 0  # 选择第一个实验进行可视化
+
+    plt.figure(figsize=(10, 6))
+    plt.imshow(np.abs(transformed_data[expt_index, channel_index, :, :]), extent=[0, data_shape[2], 1, 20],
+               cmap='jet', aspect='auto')
+    plt.xlabel('时间点')
+    plt.ylabel('频率（Hz）')
+    plt.colorbar(label='变换系数的绝对值')
+    plt.title('连续小波变换结果')
+    plt.show()
 
 
 if __name__ == '__main__':
-    # transform()
-    test()
+    transform()
+    # test()
